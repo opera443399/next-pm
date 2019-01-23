f1f132929335f9a2613579f15141c1f83e55279b
opera443399 <pc@pcswo.com>
2019-01-23 18:16:44 +0800

    feat(next): Nav and Grid exp

--- src/index.js
+++ src/index.js
@@ -1,34 +1,42 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import { Button, Icon } from '@alifd/next';
+import { Grid, Nav, Button } from '@alifd/next';
 import './index.css';
+
+const { Row, Col } = Grid;
+
+const { Item, SubNav } = Nav;
+
+const header = <span className="fusion">nextPM Demo</span>;
+const footer = <a className="login-in" href="javascript:;">Login in</a>;
+
 class App extends React.Component {
     render() {
         return (
             <div>
-                <Button.Group>
-                    <Button type="primary">OK</Button>
-                    <Button type="secondary">Cancel</Button>
-                </Button.Group>
-
-                <Button.Group>
-                    <Button disabled>Left</Button>
-                    <Button disabled>Middle</Button>
-                    <Button disabled>Right</Button>
-                </Button.Group>
-                <br />
-                <br />
-
-                <Button.Group>
-                    <Button type="primary"><Icon type="arrow-left" /> Backward</Button>
-                    <Button type="primary">Forward <Icon type="arrow-right" /></Button>
-                </Button.Group>
-
-                <Button.Group>
-                    <Button type="primary"><Icon type="prompt" /></Button>
-                    <Button type="primary"><Icon type="clock" /></Button>
-                    <Button type="primary"><Icon type="set" /></Button>
-                </Button.Group>
+                <Nav className="nav-top" direction="hoz" type="primary" header={header} footer={footer} defaultSelectedKeys={['home']} triggerType="hover">
+                    <Item key="home">Home</Item>
+                    <Item key="about">About</Item>
+                </Nav>
+                <div className="nav-left">
+                    <Row>
+                        <Col xs={4} s={4} m={4}>
+                            <Nav openMode="single">
+                                <Item>Item1</Item>
+                                <Item>Item2</Item>
+                                <Item>Item3</Item>
+                            </Nav>
+                        </Col>
+                        <Col xs={20} s={20} m={20}>
+                            <div class="ctl-btn">
+                                <Button.Group>
+                                    <Button type="primary">New</Button>
+                                    <Button type="secondary" disabled>Delete</Button>
+                                </Button.Group>
+                            </div>
+                        </Col>
+                    </Row>
+                </div>
             </div>
         );
     }
